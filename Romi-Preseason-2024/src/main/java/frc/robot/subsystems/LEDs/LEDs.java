00f938fd516e1d07caf8d42cbc8af282b713edf2
Stefan Ilic <stefan.ilic314@gmail.com>
2024-11-20 16:33:02 -0600

    finish io classes

--- Romi-Preseason-2024/src/main/java/frc/robot/subsystems/LEDs/LEDs.java
+++ Romi-Preseason-2024/src/main/java/frc/robot/subsystems/LEDs/LEDs.java
@@ -1,21 +1,23 @@
+
+
 package frc.robot.subsystems.LEDs;
 
-import edu.wpi.first.wpilibj.DigitalOutput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.LEDs.LEDsIO.LEDsIOInputs;
 
 public class LEDs extends SubsystemBase {
 
-  private LEDsIO io;
-  private LEDsIOInputs inputs;
+  LEDsIO io;
+  LEDsIOInputs inputs;
 
   public LEDs(LEDsIO io) {
     this.io = io;
-    this.inputs = new LEDsIOInputs();
+    inputs = new LEDsIOInputs();
   }
 
   @Override
   public void periodic() {
+    // This method will be called once per scheduler run
     io.updateInputs(inputs);
   }
 
@@ -23,8 +25,35 @@ public class LEDs extends SubsystemBase {
     return inputs.greenOn;
   }
 
+  public boolean getRed() {
+    return inputs.redOn;
+  }
+
+  public boolean getYellow() {
+    return inputs.yellowOn;
+  }
+
   public void greenOn() {
     io.setGreen(true);
   }
 
+  public void greenOff() {
+    io.setGreen(false);
+  }
+
+  public void redOn() {
+    io.setRed(true);
+  }
+
+  public void redOff() {
+    io.setRed(false);
+  }
+
+  public void yellowOn() {
+    io.setYellow(true);
+  }
+
+  public void yellowOff() {
+    io.setYellow(false);
+  }
 }
