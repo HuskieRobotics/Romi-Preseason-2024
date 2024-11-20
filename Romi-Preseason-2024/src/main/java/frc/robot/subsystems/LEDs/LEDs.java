93165416d44315af917e2c7032514f71c7cbeea1
Stefan Ilic <stefan.ilic314@gmail.com>
2024-11-20 16:13:09 -0600

    mid class commit

--- Romi-Preseason-2024/src/main/java/frc/robot/subsystems/LEDs/LEDs.java
+++ Romi-Preseason-2024/src/main/java/frc/robot/subsystems/LEDs/LEDs.java
@@ -2,114 +2,29 @@ package frc.robot.subsystems.LEDs;
 
 import edu.wpi.first.wpilibj.DigitalOutput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.LEDs.LEDsIO.LEDsIOInputs;
 
 public class LEDs extends SubsystemBase {
 
-  /*
-   * LED's are of the DigitalOutput class
-   *  Constructor:
-   *    DigitalOutput​(int channel)
-   *  Channels:
-   *    1 - Green
-   *    2 - Red
-   *    3 - Yellow
-   *  e.g. DigitalOutput greenLED = new DigitalOutput(1);
-   */
+  private LEDsIO io;
+  private LEDsIOInputs inputs;
 
-  //Create component instance variables:
-  DigitalOutput greenLED;
-  DigitalOutput redLED;
-  DigitalOutput yellowLED;
-  boolean greenOn = false;
-  boolean redOn = false;
-  boolean yellowOn = false;
-
-
-  //instantiate component variables in the constructor
-  public LEDs() {
-    greenLED = new DigitalOutput(1);
-    redLED = new DigitalOutput(2);
-    yellowLED = new DigitalOutput(3);
-
-    //turn off all LED's
-    turnOffGreen();
-    turnOffRed();
-    turnOffYellow();
+  public LEDs(LEDsIO io) {
+    this.io = io;
+    this.inputs = new LEDsIOInputs();
   }
-
-  // Create methods to set LED's
-
-  public void turnOnGreen() {
-    greenLED.set(true);
-    greenOn = true;
-  }
-
-  public void turnOffGreen() {
-    greenLED.set(false);
-    greenOn = false;
-  }
-
-  public void turnOnRed() {
-    redLED.set(true);
-    redOn = true;
-  }
-
-  public void turnOffRed() {
-    redLED.set(false);
-    redOn = false;
-  }
-
-  public void turnOnYellow() {
-    yellowLED.set(true);
-    yellowOn = true;
-  }
-
-  public void turnOffYellow() {
-    yellowLED.set(false);
-    yellowOn = false;
-  }
-
-  // Create methods to "get" LED's
-
-  public boolean getGreenStatus() {
-    return greenOn;
-  }
-
-  public boolean getRedStatus() {
-    return redOn;
-  }
-
-  public boolean getYellowStatus() {
-    return yellowOn;
-  }
-
-  // Alternative approach involving toggling LED's
-  // public void toggleGreen() {
-  //   if (greenOn) {
-  //     turnOffGreen();
-  //   } else {
-  //     turnOnGreen();
-  //   }
-  // }
-
-  // public void toggleRed() {
-  //   if (redOn) {
-  //     turnOffRed();
-  //   } else {
-  //     turnOnRed();
-  //   }
-  // }
-
-  // public void toggleYellow() {
-  //   if (yellowOn) {
-  //     turnOffYellow();
-  //   } else {
-  //     turnOnYellow();
-  //   }
-  // }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    io.updateInputs(inputs);
   }
+
+  public boolean getGreen() {
+    return inputs.greenOn;
+  }
+
+  public void greenOn() {
+    io.setGreen(true);
+  }
+
 }
